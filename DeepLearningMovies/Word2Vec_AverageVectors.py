c567e64aa0077fa603d9fa511fe03aab26b2c59f
Ramin Fahimi <fahimi72@gmail.com>
2015-06-30 00:51:03 +0430

    final commit

--- DeepLearningMovies/Word2Vec_AverageVectors.py
+++ DeepLearningMovies/Word2Vec_AverageVectors.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
+#  Author: Angela Chapman
+#  Date: 8/6/2014
+#
+#  This file contains code to accompany the Kaggle tutorial
+#  "Deep learning goes to the movies".  The code in this file
+#  is for Parts 2 and 3 of the tutorial, which cover how to
+#  train a model using Word2Vec.
+#
+# *************************************** #
 
 
 # ****** Read the two training sets and the test set
@@ -12,8 +21,8 @@ import logging
 import numpy as np  # Make sure that numpy is imported
 from gensim.models import Word2Vec
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import Imputer
 from KaggleWord2VecUtility import KaggleWord2VecUtility
+from sklearn.preprocessing import Imputer
 
 
 # ****** Define functions to create average word vectors
@@ -91,7 +100,7 @@ if __name__ == '__main__':
      test["review"].size, unlabeled_train["review"].size )
 
 
-	train = train[:20000]
+
     # Load the punkt tokenizer
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
@@ -102,11 +111,11 @@ if __name__ == '__main__':
     sentences = []  # Initialize an empty list of sentences
 
     print "Parsing sentences from training set"
-    #for review in train["review"]:
-     #   sentences += KaggleWord2VecUtility.review_to_sentences(review, tokenizer)
+#    for review in train["review"]:
+#        sentences += KaggleWord2VecUtility.review_to_sentences(review, tokenizer)
 
     print "Parsing sentences from unlabeled set"
-    #for review in unlabeled_train["review"]:
+    #for review in train["review"]:
      #   sentences += KaggleWord2VecUtility.review_to_sentences(review, tokenizer)
 
     # ****** Set parameters and train the word2vec model
@@ -115,88 +124,65 @@ if __name__ == '__main__':
     # creates nice output messages
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
         level=logging.INFO)
-def train(num , min_w , n ):
+
     # Set values for various parameters
-    num_features = num    # Word vector dimensionality
-    min_word_count = min_w   # Minimum word count
+    num_features = 300    # Word vector dimensionality
+    min_word_count = 40   # Minimum word count
     num_workers = 4       # Number of threads to run in parallel
     context = 10          # Context window size
     downsampling = 1e-3   # Downsample setting for frequent words
-	
+
     # Initialize and train the model (this will take some time)
     print "Training Word2Vec model..."
-    model = Word2Vec(sentences, workers=num_workers, \
-                size=num_features, min_count = min_word_count, \
-                window = context, sample = downsampling, seed=1)
+#    model = Word2Vec(sentences, workers=num_workers, \
+ #               size=num_features, min_count = min_word_count, \
+  #              window = context, sample = downsampling, seed=1)
 
     # If you don't plan to train the model any further, calling
     # init_sims will make the model much more memory-efficient.
-    #model.init_sims(replace=True)
+   # model.init_sims(replace=True)
 
     # It can be helpful to create a meaningful model name and
     # save the model for later use. You can load it later using Word2Vec.load()
     model_name = "300features_40minwords_10context"
     #model.save(model_name)
-    #model = Word2Vec.load(model_name)
+    model = Word2Vec.load(model_name)
 
-    #model.doesnt_match("man woman child kitchen".split())
-    #model.doesnt_match("france england germany berlin".split())
-    #model.doesnt_match("paris berlin london austria".split())
-    #model.most_similar("man")
-    #model.most_similar("queen")
-    #model.most_similar("awful")
+    model.doesnt_match("man woman child kitchen".split())
+    model.doesnt_match("france england germany berlin".split())
+    model.doesnt_match("paris berlin london austria".split())
+    model.most_similar("man")
+    model.most_similar("queen")
+    model.most_similar("awful")
+
 
 
     # ****** Create average vectors for the training and test sets
     #
-    #print "Creating average feature vecs for training reviews"
+    print "Creating average feature vecs for training reviews"
 
-    #trainDataVecs = getAvgFeatureVecs( getCleanReviews(train), model, num_features )
-    #trainDataVecs = Imputer().fit_transform(trainDataVecs)
-    #print trainDataVecs
-    #print "Creating average feature vecs for test reviews"
+    trainDataVecs = getAvgFeatureVecs( getCleanReviews(train), model, num_features )
 
+    print "Creating average feature vecs for test reviews"
 
-
-    testDataVecs = getAvgFeatureVecs( getCleanReviews(test[24000:]), model, num_features )
-    testDataVecs = Imputer().fit_transform(testDataVecs)
+    testDataVecs = getAvgFeatureVecs( getCleanReviews(test), model, num_features )
+    testDataVecs =  Imputer().fit_transform(testDataVecs)
+    trainDataVecs = Imputer().fit_transform(trainDataVecs)
+     
 
     # ****** Fit a random forest to the training set, then make predictions
     #
     # Fit a random forest to the training data, using 100 trees
-    forest = RandomForestClassifier( n_estimators = n )
+    forest = RandomForestClassifier( n_estimators = 100 )
 
     print "Fitting a random forest to labeled training data..."
     forest = forest.fit( trainDataVecs, train["sentiment"] )
 
     # Test & extract results
-    print 'Start Predicting'
+    print 'Start Prediction'
     result = forest.predict( testDataVecs )
 
-    print 'calculating error rate'
-    error = 0
-    for i in range(1000):
-        if( result[i] != train['sentiment'][24000+i]):
-            error += 1
-    #print error
-    print 'error rate is :' ,  error/1000.0 , 'accuracy rate is' , (1000-error)/1000.0
-
-
-
-n1 = [50 , 100]
-num1 = [100 , 200 , 300 , 400]
-max_n = [40 , 50 , 60]
-
-
-for n in n1:
-	for num in num1:
-		for m in max_n:
-			print "n , num , m : " , n , num , m
-				train(num , m , n)
-			
-
     # Write the test results
-    #output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
-    #output.to_csv( "Word2Vec_AverageVectors.csv", index=False, quoting=3 )
-    #print "Wrote Word2Vec_AverageVectors.csv"
-    
+    output = pd.DataFrame( data={"id":test["id"], "sentiment":result} )
+    output.to_csv( "Word2Vec_AverageVectors.csv", index=False, quoting=3 )
+    print "Wrote Word2Vec_AverageVectors.csv"
